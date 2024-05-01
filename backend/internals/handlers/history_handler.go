3b69029901a78799bb389c8371878fd704ad1e19
jjlim7 <jj.limmm159@gmail.com>
2024-05-02 01:28:19 +0800

    Refactor code

--- backend/internals/handlers/history_handler.go
+++ backend/internals/handlers/history_handler.go
@@ -10,7 +10,7 @@ import (
 // HistoryHandler handles requests to the /v1/history endpoint
 func HistoryHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the latest 20 saved queries from the database
-	var logs []models.DNSLog
+	var logs []models.Query
 	conn.Order("created_at desc").Limit(20).Find(&logs)
 
 	sendJSONResponse(w, http.StatusOK, logs)
---
+++ backend/models/models.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+	"gorm.io/gorm"
+)
+
+// AppInfo represents information about the application.
+type AppInfo struct {
+	Version    string `json:"version"`
+	Date       int64  `json:"date"`
+	Kubernetes bool   `json:"kubernetes"`
+}
+
+type Address struct {
+	IP string
+}
+
+type Query struct {
+	gorm.Model
+	ID        int32          `gorm:"primaryKey;autoIncrement:true"`
+	ClientIP  string         `json:"client_ip"`
+	Domain    string         `json:"domain"`
+	Addresses pq.StringArray `gorm:"type:varchar(255)[];not null" json:"addresses"`
+	CreatedAt time.Time      `gorm:"autoCreateTime:true" json:"created_at"`
+}
